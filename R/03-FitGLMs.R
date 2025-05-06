d870143773554c781279ad41012eb293f570736d
ljnewediuk <j.newedi@gmail.com>
2025-05-06 07:56:18 -0500

    finish models

--- R/03-FitGLMs.R
+++ R/03-FitGLMs.R
@@ -1,98 +1,81 @@
-# Statistical analysis
 
-# Test if lay dates are more variable within vs. outside cities
+# 03 - Fit GLM models
 
-# Response: Beta distribution CV lay dates (continuous between 0 and 1)
-# Predictor: Category (urban, rural)
-# Link function: Logit
-# Random intercepts for:
-#   Pair ID (city, species, year)
-#   Species
-#   Year
+# This script fits two models to test the hypotheses:
+#   1 - Inconsistent cues in urban areas results in more variation in laying 
+#       dates among nests in urban areas (P: CV is higher in urban than rural
+#       areas).
+#   2 - Since synchronizing reproduction with food availability, etc. is
+#       adaptive, nests with less variation in laying dates will be more 
+#       successful (P: fledgling success is lower in urban/rural areas with
+#       higher nest CV)
 
-# Model:
-# 
-# model <- glmmTMB(
-#   layDateCV ~ urban + (1 | pair_id) + (1 | species) + (1 | yr) + (1 | city),
-#   family = beta_family(link = "logit"),
-#   data = dat
-# )
+library(glmmTMB)
+library(tidyverse)
 
-# Load the data
+# 1 - Load data ====
+
+# Load the laying dates data
 lay_data <- readRDS('output/laying_data.rds')
-
-# Fit model (beta regression)
-m <- glmmTMB::glmmTMB(
-  CV ~ Land.Use + (Land.Use | City) + (Land.Use | Year) + (Land.Use | Species.Name),
-  family = glmmTMB::beta_family(link = "logit"),
-  data = lay_data
-)
-
-# Summarize
-summary(m)
-
-# Species coefficients
-coef(m)$cond$Species.Name
-
-# Test if variable lay dates reduce fledgling success
-
-# Response: Negative binomial distribution fledgling success
-# Predictor: CV lay dates
-# Link function: Log
-# Random intercepts for:
-#   Pair ID (city, species, year)
-#   Species
-#   Year
 
 # Load the reproductive success data and bind to laying data
 fledg_data <- readRDS('output/fledgling_data.rds') %>%
   na.omit() %>%
   right_join(lay_data, relationship = 'many-to-many')
 
+# 2 - Fit H1 model ====
+
+# Test if lay dates are more variable within vs. outside cities
+
+# Response: Beta distribution CV lay dates (continuous between 0 and 1)
+# Predictor: Land.Use (category: urban, rural)
+# Link function: Logit
+# Random slopes for:
+#   City
+#   Species
+#   Year
+
+# Fit model (beta regression)
+m_H1 <- glmmTMB::glmmTMB(
+  CV ~ Land.Use + (Land.Use | City) + (Land.Use | Year) + (Land.Use | Species.Name),
+  family = glmmTMB::beta_family(link = "logit"),
+  data = lay_data
+)
+
+# Summarize
+summary(m_H1)
+
+# Quickly check species coefficients
+coef(m_H1)$cond$Species.Name
+
+# Save model
+saveRDS(m_H1, 'output/laying_var_glm.rds')
+
+# 3 - Fit H2 model ====
+
+# Test if variable lay dates reduce fledgling success
+
+# Response: Zero-inflated Poisson distribution fledgling success
+# Predictor: CV lay dates
+# Link function: Log
+# Random slopes for:
+#   City
+#   Species
+#   Year
+
 # Fit model (zero-inflated poisson)
-m2 <- glmmTMB::glmmTMB(
+m_H2 <- glmmTMB::glmmTMB(
   Young.Fledged ~ CV + (CV | City) + (CV | Year) + (CV | Species.Name),
   ziformula = ~1,
   family = poisson,
   data = fledg_data
 )
 
-# Predict by species
-nd_m2_spp <- expand_grid(
-  CV = seq(from = 0, to = 0.5, by = 0.01),
-  City = NA, 
-  Year = NA, 
-  Species.Name = levels(m2$frame$Species.Name)
-)
+# Summarize
+summary(m_H2)
 
-pred_m2_spp <- predict(m2, nd_m2_spp, type = 'response')
+# Quickly check species coefficients
+coef(m_H2)$cond$Species.Name
 
-# Make data frame
-pred_m2_df_spp <- nd_m2_spp %>% 
-  mutate(Fledglings = pred_m2_spp)
-
-# Predict main effects
-nd_m2_fixef <- expand_grid(
-  CV = seq(from = 0, to = 0.5, by = 0.01),
-  City = NA, 
-  Year = NA, 
-  Species.Name = NA
-)
-
-pred_m2_fixef <- predict(m2, nd_m2_fixef, se.fit = T, type = 'response')
-
-# Make data frame
-pred_m2_df_fixef <- nd_m2_fixef %>% 
-  mutate(Fledglings = pred_m2_fixef$fit, 
-         CI = pred_m2_fixef$se.fit)
-
-
-ggplot() +
-  scale_colour_viridis_d() +
-  geom_ribbon(data = pred_m2_df_fixef,
-              aes(x = CV, ymin = Fledglings - CI, ymax = Fledglings + CI), 
-              alpha = 0.5, colour = NA) +
-  geom_line(data = pred_m2_df_fixef, aes(x = CV, y = Fledglings)) +
-  geom_line(data = pred_m2_df_spp, 
-            aes(x = CV, y = Fledglings, colour = Species.Name))
-
+# Save model
+saveRDS(m_H2, 'output/fledg_success_glm.rds')
