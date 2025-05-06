d857d87f87513ef839d200db19ee48bfe23ab64d
ljnewediuk <j.newedi@gmail.com>
2025-05-06 17:30:44 -0500

    colour by species

--- R/04-OrchardPlots.R
+++ R/04-OrchardPlots.R
@@ -31,7 +31,13 @@ m_H2 <- readRDS('output/fledg_success_glm.rds')
 r_slopes_H1 <- coef(m_H1)$cond$Species.Name %>%
   rownames_to_column('Species.Name') %>%
   select(! `(Intercept)`) %>%
-  mutate(term = 'Land.UseUrban') %>%
+  # Add new column for coloured points
+  mutate(col_pts = case_when(Species.Name == 'Prothonotary Warbler' ~ 'yellow',
+                             Species.Name == 'Purple Martin' ~ 'purple',
+                             Species.Name == 'Mountain Bluebird' ~ 'blue',
+                             Species.Name == 'Western Bluebird' ~ 'orange'),
+         # Add column for term
+         term = 'Land.UseUrban') %>%
   rename(estimate = Land.UseUrban)
 
 # Extract random slopes for species
@@ -43,7 +49,11 @@ r_slopes_H2 <- coef(m_H2)$cond$Species.Name %>%
 
 # Bind together
 r_slopes <- bind_rows(r_slopes_H1, r_slopes_H2) %>%
-  mutate(term_name = ifelse(term == 'Land.UseUrban', 'Laying date \n variation', 'Fledging \n success'))
+  # Add new column for plot labels
+  mutate(term_name = ifelse(term == 'Land.UseUrban', 'Laying date \n variation', 
+                            'Fledging \n success'),
+         # Fix point colors
+         col_pts = ifelse(is.na(col_pts), 'none', col_pts))
 
 # Extract the slope from the model with CIs
 slope_H1 <- broom.mixed::tidy(m_H1, conf.int = T) %>%
@@ -62,16 +72,18 @@ slopes <- bind_rows(slope_H1, slope_H2) %>%
 # Orchard plot panel
 ggplot() + 
   geom_hline(yintercept = 0) + 
-  geom_jitter(data = r_slopes, aes(x = term_name, y = estimate), 
-              size = 4, alpha = 0.7, colour = 'black') + 
+  geom_jitter(data = r_slopes, aes(x = term_name, y = estimate, col = col_pts, 
+                                   alpha = col_pts), size = 4) + 
+  scale_alpha_manual(values = c(1, 0.5, 1, 1, 1)) +
+  scale_colour_manual(values = c('#6593F1', 'black', '#B47762', '#F6E44C', '#403F66')) +
   theme(axis.title.y = element_blank(),
         legend.title = element_blank()) +
   geom_linerange(data = slopes, 
                  aes(x = term_name, ymin = conf.low, ymax = conf.high),
-                 lwd = 5, colour = '#ff4040') +
+                 lwd = 5, colour = 'black') +
   geom_pointrange(data = slopes,
                   aes(x = term_name, y = estimate, ymin = conf.low, ymax = conf.high),
-                  lwd = 1, shape = 21, fill = 'white', colour = '#ff4040', stroke = 7) +
+                  lwd = 1, shape = 21, fill = 'white', colour = 'black', stroke = 7) +
   coord_flip() + ylab('slope') +
   theme(panel.background = element_rect(colour = 'black', fill = 'white', linewidth = 1),
         axis.text.y = element_text(size = 18, colour = 'black'), 
@@ -86,3 +98,6 @@ ggplot() +
   ylim(-1, 1) +
   ylab('Slope estimate') 
 
+# Save as svg
+ggsave('figures/orchard.svg', plot = last_plot(), bg = 'white',
+       device = 'svg', dpi = 300, height = 12, width = 18, units = 'cm')
